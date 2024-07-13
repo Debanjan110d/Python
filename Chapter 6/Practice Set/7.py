0722147b71a93b899d7ea5de53f2c47fb0a03b0f
Debanjan110d <debanjangamedu@gmail.com>
2024-07-13 18:12:50 +0530

    Changing Some Lines

--- Chapter 6/Practice Set/7.py
+++ Chapter 6/Practice Set/7.py
@@ -1,6 +1,6 @@
 post ="Debanjan is very sexy and hot "
 
-if "Debanjan" in post:
+if ("Debanjan".lower() in post.lower()):
     print("Debanjan is in post")
 else:
     print("Debanjan is not in post")
