57e4a6ef4258dadd2305f6eacc1bc5899795880c
Debanjan110d <debanjangamedu@gmail.com>
2024-07-19 23:41:06 +0530

    Ch 10

---
+++ Chapter 10/class.py
@@ -0,0 +1,15 @@
+class Employee:
+    company = "Google"
+    salary = 1000
+    salarybonus = 500
+
+
+harry = Employee()
+rohan = Employee()
+harry.salary = 3000
+print(harry.company)
+print(harry.salary)
+print(harry.salarybonus)
+print(rohan.salary)
+print(rohan.company)
+print(rohan.salarybonus)
---
+++ Chapter 10/instance_vs_class.py
@@ -0,0 +1,11 @@
+class Employee:
+    company = "Google"
+    salary = 1000
+    salarybonus = 500
+
+
+harry = Employee()
+rohan = Employee()
+harry.salary = 3000
+print(harry.company)
+print(harry.salary)
